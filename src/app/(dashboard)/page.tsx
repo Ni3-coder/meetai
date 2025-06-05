de25e7ab729ca0eba8657aac4db1d192fdd5374b
Ni3-coder <rautn119@gmail.com>
2025-06-05 09:55:31 +0530

    trpc setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,21 +1,19 @@
+import React from 'react'
 import { auth } from '@/lib/auth';
 import { headers } from 'next/headers';
-import { HomeView } from '@/modules/home/ui/views/home-view'
-import React from 'react'
 import { redirect } from 'next/navigation';
+import { HomeView } from '@/modules/home/ui/views/home-view'
 
 const Page = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-  
-  if(!session) {
+
+  if (!session) {
     redirect("/sign-in");
   }
 
-  return (
-    <div><HomeView /></div>
-  );
+  return <HomeView />
 }
 
 export default Page;
