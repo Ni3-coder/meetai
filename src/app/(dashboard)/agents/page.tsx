2d357f6a1264666e8c3803fd21446de0fb57e5ee
Ni3-coder <rautn119@gmail.com>
2025-06-10 12:55:03 +0530

    agents filters

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,14 +1,22 @@
 import { Suspense } from "react";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import type { SearchParams } from "nuqs";
 import { redirect } from "next/navigation";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { ErrorBoundary } from "react-error-boundary";
+import { loadSearchParams } from "@/modules/agents/params";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
 import { AgentsView, AgentsViewError, AgentsViewLoading } from "@/modules/agents/ui/views/agents-view";
 
-const Page = async () => {
+interface Props {
+    searchParams: Promise<SearchParams>
+}
+
+const Page = async ({ searchParams }: Props) => {
+    const filters = await loadSearchParams(searchParams)
+
     const session = await auth.api.getSession({
         headers: await headers(),
     });
@@ -17,7 +25,9 @@ const Page = async () => {
         redirect("/sign-in");
     }
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+        ...filters
+    }));
 
     return (
         <>
