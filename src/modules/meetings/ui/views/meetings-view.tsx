ce8a0d4a3dfcfacf3c39a05d7297ada23633beca
Ni3-coder <rautn119@gmail.com>
2025-06-11 16:03:52 +0530

    mettings setup

---
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+
+export const MeetingsView = () => {
+    const trpc = useTRPC();
+    const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+
+    return (
+        <div>
+            {JSON.stringify(data)}
+        </div>
+    )
+}
+
+export const MeetingssViewLoading = () => {
+    return (
+        <LoadingState
+            title="Loading Meetings"
+            description="This may take a few seconds..."
+        />
+    )
+}
+
+export const MeetingsViewError = () => {
+    return (
+        <ErrorState
+            title="Error Loading Meeetings"
+            description="Something went wrong"
+        />
+    )
+}
