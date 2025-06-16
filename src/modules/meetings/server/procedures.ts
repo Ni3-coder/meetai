559d436625a60866eea254c75534ae31e54437df
Ni3-coder <rautn119@gmail.com>
2025-06-16 12:25:19 +0530

    meetings filters

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { db } from "@/db";
+import { MeetingStatus } from "../types";
 import { TRPCError } from "@trpc/server";
 import { agents, meetings } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
@@ -72,11 +73,21 @@ export const meetingsRouter = createTRPCRouter({
                     .min(MIN_PAGE_SIZE)
                     .max(MAX_PAGE_SIZE)
                     .default(DEFAULT_PAGE_SIZE),
-                search: z.string().nullish()
+                search: z.string().nullish(),
+                agentId: z.string().nullish(),
+                status: z
+                    .enum([
+                        MeetingStatus.Upcoming,
+                        MeetingStatus.Active,
+                        MeetingStatus.Completed,
+                        MeetingStatus.Processing,
+                        MeetingStatus.Cancelled
+                    ])
+                    .nullish(),
             })
         )
         .query(async ({ ctx, input }) => {
-            const { search, page, pageSize } = input;
+            const { search, page, pageSize, status, agentId } = input;
 
             const data = await db
                 .select({
@@ -90,6 +101,8 @@ export const meetingsRouter = createTRPCRouter({
                     and(
                         eq(meetings.userId, ctx.auth.user.id),
                         search ? ilike(meetings.name, `%${search}%`) : undefined,
+                        status ? eq(meetings.status, status) : undefined,
+                        agentId ? eq(meetings.agentId, agentId) : undefined
                     )
                 )
                 .orderBy(desc(meetings.createdAt), desc(meetings.id))
@@ -104,6 +117,8 @@ export const meetingsRouter = createTRPCRouter({
                     and(
                         eq(meetings.userId, ctx.auth.user.id),
                         search ? ilike(meetings.name, `%${search}%`) : undefined,
+                        status ? eq(meetings.status, status) : undefined,
+                        agentId ? eq(meetings.agentId, agentId) : undefined
                     )
                 );
 
